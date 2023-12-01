e0c72f611de5530eb40f3f84c2e8946b206dee71
decotart <dfyrkin@bk.ru>
2023-12-01 19:14:56 +0300

    Добавьте файлы проекта.

---
+++ Практическая работа №9/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа__9
+{
+    public struct MailSends
+    {
+        private string _town,
+            _street,
+            _recipient;
+
+        private int _house,
+            _apps,
+            _count;
+
+        public string Town
+        {
+            get { return _town; }
+            set { _town = value; }
+        }
+
+        public string Street
+        {
+            get { return _street; }
+            set { _street = value; }
+        }
+
+        public string Recipient
+        {
+            get { return _recipient; }
+            set { _recipient = value; }
+        }
+
+        public int House
+        {
+            get { return _house; }
+            set
+            {
+                if (value > 0)
+                {
+                    _house = value;
+                }
+                else throw new ArgumentException("Номер дома не может быть отрицательным.");
+            }
+        }
+
+        public int Apps
+        {
+            get { return _apps; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _apps = value;
+                }
+                else throw new ArgumentException("Номер квартиры не может быть отрицательным.");
+            }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _count = value;
+                }
+                else throw new ArgumentException("Количество отправленных посылок не может быть меньше нуля");
+            }
+        }
+
+        public MailSends()
+        {
+            _town = "Введите город";
+            _street = "Введите улицу";
+            _recipient = "Введите получателя";
+
+            _house = 0;
+            _apps = 0;
+            _count = 0;
+        }
+
+        public MailSends(string town, string street, string recipient, int house, int apps, int count)
+        {
+            if (house <= 0 || apps <= 0 || count < 0)
+            {
+                throw new ArgumentException("Некорректные значения");
+            }
+            else
+            {
+                _town = town;
+                _street = street;
+                _recipient = recipient;
+
+                _apps = apps;
+                _count = count;
+                _house = house;
+            }
+        }
+
+        public static int GetResult(List<MailSends> list, string town)
+        {
+            int result = 0;
+
+            foreach (MailSends i in list)
+            {
+                if (i.Town == town)
+                {
+                    result += i.Count;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Практическая работа №9/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_работа__9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<MailSends> mainList = new List<MailSends>();
+
+        MailSends m1 = new(),
+            m2 = new(),
+            m3 = new(),
+            m4 = new(),
+            m5 = new();
+
+        private void dataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainList.Add(m1);
+            mainList.Add(m2);
+            mainList.Add(m3);
+            mainList.Add(m4);
+            mainList.Add(m5);
+
+            dataGrid.ItemsSource = mainList;
+        }
+
+        private void dataGrid_CellEditing(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            int columnIndex = e.Column.DisplayIndex,
+                rowIndex = e.Row.GetIndex();
+
+            try
+            {
+                MailSends temp = mainList[rowIndex];
+
+                switch (columnIndex)
+                {
+                    case 0:
+                        temp.Town = ((TextBox)e.EditingElement).Text;
+                        break;
+                    case 1:
+                        temp.Street = ((TextBox)e.EditingElement).Text;
+                        break;
+                    case 2:
+                        temp.Recipient = ((TextBox)e.EditingElement).Text;
+                        break;
+                    case 3:
+                        temp.House = Convert.ToInt32(((TextBox)e.EditingElement).Text);
+                        break;
+                    case 4:
+                        temp.Apps = Convert.ToInt32(((TextBox)e.EditingElement).Text);
+                        break;
+                    case 5:
+                        temp.Count = Convert.ToInt32(((TextBox)e.EditingElement).Text);
+                        break;
+                }
+                mainList[rowIndex] = temp;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод");
+                ((TextBox)e.EditingElement).Text = "0";
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+                ((TextBox)e.EditingElement).Text = "0";
+            }
+        }
+
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+            int result = MailSends.GetResult(mainList, tbTown.Text);
+
+            tbResult.Text = result.ToString();
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Бла-бла-бла");
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
